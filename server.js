83ac8ebbf45214f89e372d9e81563770f62d72bf
Dean <deank242@gmail.com>
2022-05-11 19:57:51 -0400

    set up npm packages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,25 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: process.env.DB_HOST,
+      // Your MySQL username,
+      user: process.env.DB_USER,
+      // Your MySQL password
+      password: process.env.DB_PASS,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.get('/', (req, res) => {
     res.json({
